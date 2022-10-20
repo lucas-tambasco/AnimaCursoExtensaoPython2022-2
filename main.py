3c2fd5ed2c773f0aa84f4f073c67ed40ecacb687
Lucas Tambasco <78384108+lucastambasco-star@users.noreply.github.com>
2022-10-20 00:53:39 +0000

    Aprendi a criar variáveis em Python e exibi-las na tela!

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-#Meu primeiro projeto!
-#print() = comando de saída
+# Meu primeiro projeto!
+#
+# print() = comando de saída
 print("Alo mundo!")
+
+nome = "Lucas Gomes Tambasco"
+idade = 18
+
+print(nome)
+
+
 '''
 Comentario em bloco
 '''
