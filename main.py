e189a4143f88e0956d69b9c73d13f2f789819d1d
Lucas Tambasco <78384108+lucastambasco-star@users.noreply.github.com>
2022-10-20 01:19:28 +0000

    Aprendi 3 maneiras de concatenar String com numeros inteiros

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ idade = 18
 
 print(nome)
 
+print("A minha idade é "+str(idade)+"anos")
+print(f"A minha idade é {idade} anos")
+print("A minha idade é {} anos".format(idade))
+
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
 
 '''
 Comentario em bloco
